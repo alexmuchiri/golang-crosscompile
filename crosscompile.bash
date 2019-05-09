0a6c6f10a767826c4817ce51220c157bd28e23b2
professor wonderful <31970058+alexmuchiri@users.noreply.github.com>
2019-05-09 17:53:53 +0300

    Update crosscompile.bash

--- crosscompile.bash
+++ crosscompile.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright 2012 The Go Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style
+# Use of this source code is governed by a BSD-style 
 # license that can be found in the LICENSE file.
 
 # support functions for go cross compilation
